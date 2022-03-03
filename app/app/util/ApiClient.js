eebd98ee5fb677cf0ca29e28d8f5cd37393eba69
PPYang <PPYang@gopluslabs.io>
2022-03-03 18:25:22 +0800

    Fix code clutter

--- app/app/util/ApiClient.js
+++ app/app/util/ApiClient.js
@@ -20,165 +20,167 @@ const RELEASE_INVITE_URL = 'https://community.gopocket.xyz';
 const log = util.logInfo;
 let fetch_config_success = false;
 
-const fetchConfig = async() => {
-    const configUrl = util.useTestServer() ?
-        'https://api.beta.gopocket.finance/app/config' :
-        'https://api.gopocket.finance/app/config';
+const fetchConfig = async () => {
+	const configUrl = util.useTestServer()
+		? 'https://api.beta.gopocket.finance/app/config'
+		: 'https://api.gopocket.finance/app/config';
 
-    try {
-        const response = await fetch(configUrl);
-        if (response.status === 200) {
-            try {
-                const rawData = await response.text();
-                const content = await decryptString(JSON.parse(rawData).data);
-                const jsonContent = JSON.parse(content);
-                const updateConfig = jsonContent.update_config;
-                const chainTypes = jsonContent.chain_types_v2;
-                const appstoreBaseVersion = jsonContent.appstore_base_version;
-                const dappPage = jsonContent.dapp_page;
-                const contractList = jsonContent.contract_list;
-                const useOffchainEndPoint = jsonContent.use_offchain_endpoint;
-                const ipfsGateway = jsonContent.ipfs_gateway;
-                const famousAccounts = jsonContent.famous_accounts;
-                if (updateConfig) {
-                    const config = Device.isAndroid() ? updateConfig.android : updateConfig.iphone;
-                    store.dispatch(SetUpdateConfig(config));
-                }
-                if (chainTypes) {
-                    store.dispatch(updateDefaultTypes(chainTypes));
-                }
-                if (appstoreBaseVersion) {
-                    store.dispatch(SetAppstoreBaseVersion(appstoreBaseVersion));
-                }
-                if (dappPage) {
-                    store.dispatch(updateDappPage(dappPage));
-                    NativeThreads.get().callSqliteAsync('updateWhitelistDapps', getDapp(dappPage));
-                    const lDapp = getLanguageDapp(dappPage);
-                    lDapp ? .favourites && store.dispatch(addFavouriteDapps(lDapp ? .favourites));
-                }
-                if (contractList) {
-                    store.dispatch(updateContractList(contractList));
-                }
-                if (famousAccounts) {
-                    store.dispatch(updateFamousAccounts(famousAccounts));
-                }
-                if (ipfsGateway) {
-                    Engine.context.CollectiblesController.setIpfsGateway(ipfsGateway);
-                }
-                if (Device.isIos()) {
-                    const appstoreBaseVersion = Number(store.getState().settings.appstoreBaseVersion);
-                    global.shouldHideSthForAppStoreReviewer = !appstoreBaseVersion || Number(getAppVersionCode()) >= appstoreBaseVersion;
-                }
-                global.useOffchainEndPoint = !!useOffchainEndPoint;
-            } finally {
-                fetch_config_success = true;
-                util.logDebug('leon.w@fetch_config_success');
-            }
-        }
-    } catch (e) {
-        util.logDebug('cyh fetch config fail, e', e);
-    }
-    if (!fetch_config_success) {
-        setTimeout(() => fetchConfig(), 5000);
-    }
+	try {
+		const response = await fetch(configUrl);
+		if (response.status === 200) {
+			try {
+				const rawData = await response.text();
+				const content = await decryptString(JSON.parse(rawData).data);
+				const jsonContent = JSON.parse(content);
+				const updateConfig = jsonContent.update_config;
+				const chainTypes = jsonContent.chain_types_v2;
+				const appstoreBaseVersion = jsonContent.appstore_base_version;
+				const dappPage = jsonContent.dapp_page;
+				const contractList = jsonContent.contract_list;
+				const useOffchainEndPoint = jsonContent.use_offchain_endpoint;
+				const ipfsGateway = jsonContent.ipfs_gateway;
+				const famousAccounts = jsonContent.famous_accounts;
+				if (updateConfig) {
+					const config = Device.isAndroid() ? updateConfig.android : updateConfig.iphone;
+					store.dispatch(SetUpdateConfig(config));
+				}
+				if (chainTypes) {
+					store.dispatch(updateDefaultTypes(chainTypes));
+				}
+				if (appstoreBaseVersion) {
+					store.dispatch(SetAppstoreBaseVersion(appstoreBaseVersion));
+				}
+				if (dappPage) {
+					store.dispatch(updateDappPage(dappPage));
+					NativeThreads.get().callSqliteAsync('updateWhitelistDapps', getDapp(dappPage));
+					const lDapp = getLanguageDapp(dappPage);
+					lDapp?.favourites && store.dispatch(addFavouriteDapps(lDapp?.favourites));
+				}
+				if (contractList) {
+					store.dispatch(updateContractList(contractList));
+				}
+				if (famousAccounts) {
+					store.dispatch(updateFamousAccounts(famousAccounts));
+				}
+				if (ipfsGateway) {
+					Engine.context.CollectiblesController.setIpfsGateway(ipfsGateway);
+				}
+				if (Device.isIos()) {
+					const appstoreBaseVersion = Number(store.getState().settings.appstoreBaseVersion);
+					global.shouldHideSthForAppStoreReviewer =
+						!appstoreBaseVersion || Number(getAppVersionCode()) >= appstoreBaseVersion;
+				}
+				global.useOffchainEndPoint = !!useOffchainEndPoint;
+			} finally {
+				fetch_config_success = true;
+				util.logDebug('leon.w@fetch_config_success');
+			}
+		}
+	} catch (e) {
+		util.logDebug('cyh fetch config fail, e', e);
+	}
+	if (!fetch_config_success) {
+		setTimeout(() => fetchConfig(), 5000);
+	}
 };
 
 export async function initApiClient() {
-    global.channel = await PreventScreenshot.getChannel();
-    global.appVersion = await getVersion();
-    global.appVersionCode = await getBuildNumber();
-    global.deviceInfo = await new Promise(resolve => {
-        if (Platform.OS === 'android') {
-            NativeModules.RNToolsManager.getDeviceInfo().then(event => {
-                resolve(event);
-            });
-            return;
-        }
-        getIosDeviceInfo().then(info => {
-            resolve(info);
-        });
-    });
-    if (Platform.OS === 'android') {
-        const deviceInfo = await getInfo();
-        try {
-            global.deviceId = JSON.parse(deviceInfo.message).device_id;
-        } catch (e) {
-            global.deviceId = '';
-        }
-        global.testFlight = false;
-        global.shouldHideSthForAppStoreReviewer = false;
-    } else {
-        global.deviceId = '';
-        global.testFlight = await isTestFlight();
-        const appstoreBaseVersion = Number(store.getState().settings.appstoreBaseVersion);
-        global.shouldHideSthForAppStoreReviewer = !appstoreBaseVersion || Number(getAppVersionCode()) >= appstoreBaseVersion;
-    }
+	global.channel = await PreventScreenshot.getChannel();
+	global.appVersion = await getVersion();
+	global.appVersionCode = await getBuildNumber();
+	global.deviceInfo = await new Promise(resolve => {
+		if (Platform.OS === 'android') {
+			NativeModules.RNToolsManager.getDeviceInfo().then(event => {
+				resolve(event);
+			});
+			return;
+		}
+		getIosDeviceInfo().then(info => {
+			resolve(info);
+		});
+	});
+	if (Platform.OS === 'android') {
+		const deviceInfo = await getInfo();
+		try {
+			global.deviceId = JSON.parse(deviceInfo.message).device_id;
+		} catch (e) {
+			global.deviceId = '';
+		}
+		global.testFlight = false;
+		global.shouldHideSthForAppStoreReviewer = false;
+	} else {
+		global.deviceId = '';
+		global.testFlight = await isTestFlight();
+		const appstoreBaseVersion = Number(store.getState().settings.appstoreBaseVersion);
+		global.shouldHideSthForAppStoreReviewer =
+			!appstoreBaseVersion || Number(getAppVersionCode()) >= appstoreBaseVersion;
+	}
 
-    global.useOffchainEndPoint = false;
-    await fetchConfig();
-    util.checkEtherscanAvailable('0xd8da6bf26964af9d7eed9e03e53415d37aa96045', ETHERSCAN_APIKEYS[0]).then(
-        etherscanAvailable => {
-            NativeThreads.get().callEngineAsync('setEtherscanAvailable', etherscanAvailable);
-            util.setEtherscanAvailable(etherscanAvailable);
-            util.logDebug(`leon.w@etherscanAvailable=${etherscanAvailable}`);
-        }
-    );
+	global.useOffchainEndPoint = false;
+	await fetchConfig();
+	util.checkEtherscanAvailable('0xd8da6bf26964af9d7eed9e03e53415d37aa96045', ETHERSCAN_APIKEYS[0]).then(
+		etherscanAvailable => {
+			NativeThreads.get().callEngineAsync('setEtherscanAvailable', etherscanAvailable);
+			util.setEtherscanAvailable(etherscanAvailable);
+			util.logDebug(`leon.w@etherscanAvailable=${etherscanAvailable}`);
+		}
+	);
 
-    log(
-        'initApiClient -> ' +
-        JSON.stringify({
-            API_KEY,
-            deviceId: global.deviceId,
-            channel: global.channel,
-            appVersion: global.appVersion,
-            appVersionCode: global.appVersionCode,
-            deviceInfo: global.deviceInfo,
-            testFlight: global.testFlight,
-            useOffchainEndPoint: global.useOffchainEndPoint,
-            shouldHideSthForAppStoreReviewer: global.shouldHideSthForAppStoreReviewer
-        })
-    );
+	log(
+		'initApiClient -> ' +
+			JSON.stringify({
+				API_KEY,
+				deviceId: global.deviceId,
+				channel: global.channel,
+				appVersion: global.appVersion,
+				appVersionCode: global.appVersionCode,
+				deviceInfo: global.deviceInfo,
+				testFlight: global.testFlight,
+				useOffchainEndPoint: global.useOffchainEndPoint,
+				shouldHideSthForAppStoreReviewer: global.shouldHideSthForAppStoreReviewer
+			})
+	);
 }
 
 export function getDeviceId() {
-    return global.deviceId;
+	return global.deviceId;
 }
 
 export function getChannel() {
-    return global.channel;
+	return global.channel;
 }
 
 export function getAppVersion() {
-    return global.appVersion;
+	return global.appVersion;
 }
 
 export function getAppVersionCode() {
-    return global.appVersionCode;
+	return global.appVersionCode;
 }
 
 export function getDeviceInfo() {
-    return global.deviceInfo;
+	return global.deviceInfo;
 }
 
 export function getTestFlight() {
-    return global.testFlight;
+	return global.testFlight;
 }
 
 export function getInviteUrl() {
-    const isTest = util.useTestServer();
-    return isTest ? TEST_INVITE_URL : RELEASE_INVITE_URL;
+	const isTest = util.useTestServer();
+	return isTest ? TEST_INVITE_URL : RELEASE_INVITE_URL;
 }
 
 export function shouldHideSthForAppStoreReviewer() {
-    return global.shouldHideSthForAppStoreReviewer;
+	return global.shouldHideSthForAppStoreReviewer;
 }
 
 export async function useOffchainEndPoint() {
-    let try_count = 8;
-    // eslint-disable-next-line no-unmodified-loop-condition
-    while (!fetch_config_success && try_count > 0) {
-        await new Promise(resolve => setTimeout(() => resolve(true), 500));
-        try_count -= 1;
-    }
-    return global.useOffchainEndPoint;
+	let try_count = 8;
+	// eslint-disable-next-line no-unmodified-loop-condition
+	while (!fetch_config_success && try_count > 0) {
+		await new Promise(resolve => setTimeout(() => resolve(true), 500));
+		try_count -= 1;
+	}
+	return global.useOffchainEndPoint;
 }
