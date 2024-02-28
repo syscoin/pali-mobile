8698486a4ecb528087b607c6b2c9dd48ec04320b
thalesbmc <thalesbmc@gmail.com>
2024-02-28 14:08:59 -0300

    feat: gets the theme from system

--- app/app/theme/ThemeProvider.js
+++ app/app/theme/ThemeProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Appearance, AppearanceProvider, useColorScheme } from 'react-native'; // Import Appearance and useColorScheme
 
 // Create a context for the theme
 export const ThemeContext = createContext();
@@ -15,14 +16,19 @@ export const useTheme = () => {
 
 // Create a provider component
 export const ThemeProvider = ({ children }) => {
-	const [theme, setTheme] = useState('light'); // default theme
+	const systemPreference = useColorScheme();
+	const [theme, setTheme] = useState(systemPreference); // default theme
 
 	// Load the theme from AsyncStorage when the provider mounts
 	useEffect(() => {
 		const loadTheme = async () => {
 			const storedTheme = await AsyncStorage.getItem('theme');
 			if (storedTheme) {
+				console.log(storedTheme);
 				setTheme(storedTheme);
+			} else {
+				console.log(systemPreference, 'wow');
+				setTheme(systemPreference);
 			}
 		};
 		loadTheme();
